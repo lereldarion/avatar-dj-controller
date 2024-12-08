f48614d8c54d888b7ebd4854f3e0dda04b9646b2
Lereldarion <131812197+lereldarion@users.noreply.github.com>
2024-12-08 09:56:14 +0100

    Prototype avatar controls for Slider / Button / Potentiometer
    Simple test meshes, using vertex color and a diffuse shader only.
    
    Button : squish physbone. Can be merged between buttons.
    Clever skinning is used to make a blue color appear when the button is pressed.
    
    Slider : long "lever" physbone, with a 1D position constraint to straighten the slider handle.
    Physbone hinge limit is set to `arcsin(physbone len / slider range)`.
    A previous version tried squish & stretch, but it was not working correctly with collisions (only squish worked).
    
    Potentiometer : use contact to drive rotation constraints that track hand rotation.
    Drive potentiometer rotation through a inside physbone collider + hinge limited physbone, in order to have a hinge range limit.
    Will need 2 contacts for both hands per potentiometer.

---
+++ Assets/Avatar/DJ/LuciferDJ.cs
@@ -0,0 +1,150 @@
+#if UNITY_EDITOR
+using AnimatorAsCode.V1;
+using AnimatorAsCode.V1.ModularAvatar;
+using nadena.dev.ndmf;
+using nadena.dev.modular_avatar.core;
+using UnityEditor;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
+using VRC.SDKBase;
+using VRC.SDK3.Dynamics.Constraint.Components;
+using AnimatorAsCode.V1.VRC;
+using VRC.SDK3.Dynamics.Contact.Components;
+using VRC.Dynamics;
+
+
+[assembly: ExportsPlugin(typeof(Lereldarion.LuciferDJPlugin))]
+
+namespace Lereldarion {
+    public class LuciferDJ : MonoBehaviour, IEditorOnly {
+        public VRCExpressionsMenu menu_target;
+
+        [Header("DJ Controller")]
+        public VRCParentConstraint dj_controller;
+
+        public VRCRotationConstraint potentiometer_driver;
+        public VRCRotationConstraint potentiometer_hand_tracker;
+        public VRCContactReceiver potentiometer_contact;
+    }
+
+    public class LuciferDJPlugin : Plugin<LuciferDJPlugin> {
+        public override string DisplayName => "Lucifer DJ Animator";
+
+        public string SystemName => "LuciferDJ";
+
+        protected override void Configure() {
+            InPhase(BuildPhase.Generating).Run(DisplayName, Generate);
+        }
+
+        private void Generate(BuildContext ctx) {
+            var config = ctx.AvatarRootTransform.GetComponentInChildren<LuciferDJ>(true);
+            if(config == null) { return; }
+
+            var aac = AacV1.Create(new AacConfiguration {
+                SystemName = SystemName,
+                AnimatorRoot = ctx.AvatarRootTransform,
+                DefaultValueRoot = ctx.AvatarRootTransform,
+                AssetKey = GUID.Generate().ToString(),
+                AssetContainer = ctx.AssetContainer,
+                ContainerMode = AacConfiguration.Container.OnlyWhenPersistenceRequired,
+                DefaultsProvider = new AacDefaultsProvider()
+            });
+            
+            var ma_object = new GameObject(SystemName) { transform = { parent = ctx.AvatarRootTransform }};
+            var ma = MaAc.Create(ma_object);
+            MaacMenuItem new_installed_menu_item() {
+                var menu = new GameObject { transform = { parent = ma_object.transform }};
+                var installer = menu.AddComponent<ModularAvatarMenuInstaller>();
+                installer.installTargetMenu = config.menu_target;
+                return ma.EditMenuItem(menu);
+            }
+            var ctrl = aac.NewAnimatorController();
+
+            {
+                var layer = ctrl.NewLayer("DJ");
+
+                var dj_enabled = layer.BoolParameter("DJ/Enabled");
+                ma.NewParameter(dj_enabled).WithDefaultValue(false).NotSaved();
+                new_installed_menu_item().Name("DJ").Toggle(dj_enabled);
+
+                var dj_world = layer.BoolParameter("DJ/World");
+                ma.NewParameter(dj_world).WithDefaultValue(false).NotSaved();
+                new_installed_menu_item().Name("DJ World").Toggle(dj_world);
+
+                var disabled = layer.NewState("Disabled");
+                disabled.Drives(dj_world, false).DrivingLocally(); // Reset, 2 bools but 3 states
+                disabled.WithAnimation(aac.NewClip()
+                    .Toggling(config.dj_controller.gameObject, false)
+                    .Animating(SetConstraintWorldFixed(config.dj_controller, false))
+                );
+
+                var enabled = layer.NewState("Enabled");
+                enabled.Drives(dj_world, false).DrivingLocally();
+                enabled.WithAnimation(aac.NewClip()
+                    .Toggling(config.dj_controller.gameObject, true)
+                    .Animating(SetConstraintWorldFixed(config.dj_controller, false))
+                );
+
+                var world = layer.NewState("World");
+                world.WithAnimation(aac.NewClip()
+                    .Toggling(config.dj_controller.gameObject, true)
+                    .Animating(SetConstraintWorldFixed(config.dj_controller, true))
+                );
+
+                layer.AnyTransitionsTo(disabled).When(dj_enabled.IsFalse());
+
+                // ignore Late sync <=> bed enabled+world
+                disabled.TransitionsTo(enabled).When(dj_enabled.IsTrue()).And(dj_world.IsFalse());
+
+                enabled.TransitionsTo(world).When(dj_enabled.IsTrue()).And(dj_world.IsTrue());
+                world.TransitionsTo(enabled).When(dj_world.IsFalse());
+            }
+            // Potentiometer system
+            {
+                var layer = ctrl.NewLayer("DJ/Potentiometer");
+
+                var dj_enabled = layer.BoolParameter("DJ/Enabled");
+                var contact = layer.BoolParameter(config.potentiometer_contact.parameter);
+
+                var disabled = layer.NewState("Disabled");
+                disabled.WithAnimation(aac.NewClip()
+                    .Animating(SetConstraintActive(config.potentiometer_driver, false))
+                    .Animating(SetConstraintActive(config.potentiometer_hand_tracker, false))
+                );
+
+                var standby = layer.NewState("Standby");
+                standby.WithAnimation(aac.NewClip()
+                    .Animating(SetConstraintActive(config.potentiometer_driver, false))
+                    .Animating(SetConstraintActive(config.potentiometer_hand_tracker, true))
+                );
+
+                var grabbed = layer.NewState("Grabbed");
+                grabbed.WithAnimation(aac.NewClip()
+                    .Animating(SetConstraintActive(config.potentiometer_driver, true))
+                    .Animating(SetConstraintActive(config.potentiometer_hand_tracker, false))
+                );
+
+                disabled.TransitionsTo(standby).When(dj_enabled.IsTrue());
+                layer.AnyTransitionsTo(disabled).When(dj_enabled.IsFalse());
+
+                standby.TransitionsTo(grabbed).When(contact.IsTrue());
+                grabbed.TransitionsTo(standby).When(contact.IsFalse());
+            }
+            ma.NewMergeAnimator(ctrl.AnimatorController, VRCAvatarDescriptor.AnimLayerType.FX);
+        }
+
+        static private System.Action<AacFlEditClip> SetConstraintWorldFixed(VRCConstraintBase constraint, bool fixed_to_world) {
+            return clip => {
+                clip.Animates(constraint, "FreezeToWorld").WithOneFrame(fixed_to_world ? 1 : 0);
+            };
+        }
+
+        static private System.Action<AacFlEditClip> SetConstraintActive(VRCConstraintBase constraint, bool active) {
+            return clip => {
+                clip.Animates(constraint, "IsActive").WithOneFrame(active ? 1 : 0);
+            };
+        }
+    }
+}
+#endif
--- Readme.md
+++ Readme.md
@@ -4,3 +4,6 @@ Strategy :
 - Extract slider/etc positions to pixels on the desktop view
 - Rust deamon to read the pixels and push midi events to whatever
 - NDMF script with AAC to process the DJ pad, generate the tracking mesh, and a JSON descriptor
+
+Beware of post-processing for pixel outputs.
+Solution : huge signal/noise ratio by having 1bit/pixel, should work even with extreme PP.
